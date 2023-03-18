303b76d6f8211a626cd16eb6221dd5f9a2d4e4bb
luvasd <128108448+luvasd@users.noreply.github.com>
2023-03-18 04:00:54 -0300

    Create exercicio01.py

---
+++ semana01/exercicio01.py
@@ -0,0 +1,29 @@
+nome = "lucas Descovi"
+ 
+idade = 18
+
+peso = 70
+
+print("nome:", nome)
+print("idade:", idade)
+print("peso:", peso,"kg")
+
+cpf = "777.777.666-15"
+print("cpf:", cpf)
+
+dataDeNascimento = "14 08 2004"
+print("nesceu:", dataDeNascimento)
+
+cidade = "Nova Palma"
+
+estado = "Rio Grande Do Sul"
+print("cidade:", cidade)
+print("estado:", estado)
+
+velocidade = "100km"
+
+preco = 7.49
+
+print("velocidade:", velocidade)
+
+print("preço:", preco)
