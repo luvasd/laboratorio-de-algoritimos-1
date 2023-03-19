23e3c350eb18dd74232f013edb08dc0c18ec571c
luvasd <128108448+luvasd@users.noreply.github.com>
2023-03-18 21:02:34 -0300

    Create exercicio02.py

---
+++ semana02/exercicio02.py
@@ -0,0 +1,9 @@
+digiteAltura = float(input("digite a altura do retangulo:" ))
+digiteBase = float(input("digite a base do retangulo:" ))
+
+A = digiteAltura * digiteBase 
+print("A area do retangulo é:", A )
+
+P = digiteAltura * 2 + digiteBase * 2 
+
+print("O perimetro do retangulo é: ", P)
