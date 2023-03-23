c720e88de5c6cd2cdcc5861734b91714f899d9d1
luvasd <128108448+luvasd@users.noreply.github.com>
2023-03-23 04:55:52 -0300

    Update exercicio07.py

--- semana02/exercicio07.py
+++ semana02/exercicio07.py
@@ -1,14 +1,14 @@
 ### Uma empresa deseja trocar os pneus de toda frota de carros. Sabe-se que cada pneu custa R$395,40. Com isso, leia o total de carros e apresente o valor final.
 ##Utilize a função inpuT.####
 
-todaFrotaDeCarros = int(input("Digite quantos carros tem a forta da sua empresa:" ))
+todaFrotaDeCarros  =  int ( input ( "Digite quantos carros tem a forta da sua empresa:" ))
 
-pneu = 5
+pneu  =  5
 
-peneuCusta = 395.40
+peneuCusta  =  395.40
 
-valorGasto = pneu * peneuCusta
+valorGasto  =  pneu  *  peneuCusta
 
-valorFinal = valorGasto * todaFrotaDeCarros
+valorFinal  =  valorGasto  *  todaFrotaDeCarros
 
-print("Sua empresa vai gastar trocando os peneus de toda forta o valor de:", valorFinal)
+print ( "Sua empresa vai gastar trocando os peneus de toda forta o valor de:" , valorFinal )
