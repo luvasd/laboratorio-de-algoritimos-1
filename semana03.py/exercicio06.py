d22ab29d4b65d017e7bd0705e5d6420cd2fa0f9f
luvasd <128108448+luvasd@users.noreply.github.com>
2023-03-25 02:53:48 -0300

    Create exercicio06.py

---
+++ semana03.py/exercicio06.py
@@ -0,0 +1,13 @@
+n1 = float(input("Digite um numero:" )) 
+
+nNegativo = n1 < 0
+
+if nNegativo:
+    print("O numero é invalido")
+
+else: 
+    nNegativo = n1 >= 0
+
+    dobro = n1 * 2
+
+    print("O dobro do numero é:", dobro) 
