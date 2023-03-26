1dcd3ce243bfe0e9f69a771d2d0d51bccf8ba1de
luvasd <128108448+luvasd@users.noreply.github.com>
2023-03-26 01:04:20 -0300

    Create exercicio04.py

---
+++ semana03.py/exercicio04.py
@@ -0,0 +1,7 @@
+litroGasosa = float(input("Digite o preço do litro de gasolina:" ))
+
+quantoPagou = float(input("Digite quanto vc pagou para enxer o tanque:" ))
+
+litrosAbastecidos = (quantoPagou / litroGasosa) 
+
+print("Voçe colocou", litrosAbastecidos, "litros de gasolina.")
