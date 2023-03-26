d126286cbafb77b13775feb416899e339aa7f1d4
luvasd <128108448+luvasd@users.noreply.github.com>
2023-03-26 01:05:39 -0300

    Create exercicio02.py

---
+++ semana03.py/exercicio02.py
@@ -0,0 +1,12 @@
+n1 = int(input("Digite um número:" ))
+
+n2 = int(input("Digite um número:" ))
+
+distinto = n1 != n2
+
+if distinto:
+    nDistinto = n1 < n2
+    print(n1, n2)
+
+else:
+    print(n2, n1)
