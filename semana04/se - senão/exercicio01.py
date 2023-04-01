cf0fd849f74051e8164d407e915a2da6c187d7e5
luvasd <128108448+luvasd@users.noreply.github.com>
2023-04-01 02:32:37 -0300

    Create exercicio01.py

---
+++ semana04/se - senão/exercicio01.py
@@ -0,0 +1,10 @@
+valor1 = int(input("Digite um valor: " ))
+
+valor2 = int(input("Digite o segundo valor: " ))
+
+if valor1 > valor2:
+    print("O primeiro valor é p maior")
+elif valor1 < valor2:
+    print("O segundo valor é maior")
+else:
+    print("Eles são iguais, por favor digite numeros diferentes")
