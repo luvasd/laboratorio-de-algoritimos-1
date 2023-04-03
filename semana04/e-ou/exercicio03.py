fccfc7601fb51359b01d0a570e78538e4a579a94
luvasd <128108448+luvasd@users.noreply.github.com>
2023-04-03 00:26:21 -0300

    Create exercicio03.py

---
+++ semana04/e-ou/exercicio03.py
@@ -0,0 +1,14 @@
+nota1 = float(input("Digite a sua primeira nota: "))
+
+nota2 = float(input("Digite a sua segunda nota: "))
+
+frequencia = float(input("Digite a sua frequência/presença: "))
+
+mediaNota = (nota1 + nota2) / 2 
+
+if mediaNota >= 7.0 and frequencia >= 75:
+    print("Vc esta Aprovado")
+elif mediaNota >= 4.0 and mediaNota < 7.0 and frequencia > 75:
+    print("Vc entá em Exame")
+elif mediaNota < 4.0 or frequencia < 75:
+    print("Vc entá Reprovado")
