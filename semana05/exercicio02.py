233f24306f2ee51e7a470a2c70e5a0395ea2cbed
luvasd <128108448+luvasd@users.noreply.github.com>
2023-04-28 16:32:04 -0300

    Create exercicio02.py

---
+++ semana05/exercicio02.py
@@ -0,0 +1,9 @@
+totalDe10 = 0
+numero = 1
+
+while numero != 0:
+    numero = float(input("informe um numero: " ))
+    if numero == 10:
+        totalDe10 += 1
+
+print("O totas de vezes q o numero 10 apareceu foram: ", totalDe10)    
