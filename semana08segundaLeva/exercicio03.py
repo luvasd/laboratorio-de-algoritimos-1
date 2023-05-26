ded8ab5b368c12fec41c9205f57afa495b4b8c51
luvasd <128108448+luvasd@users.noreply.github.com>
2023-05-26 16:58:46 -0300

    Create exercicio03.py

---
+++ semana08segundaLeva/exercicio03.py
@@ -0,0 +1,10 @@
+def dobro(n1, n2):
+    #dobro1 = n1 * 2
+    #dobro2 = n2 * 2
+    return n1 * 2,n2 * 2 #esse é o jeito mains rapido de fazer com menas linhas no codigo
+
+def main():
+    recebeDobro =  print(dobro(3,5))
+
+main()
+#tem q mudar o parametro dentro do defMain() OK...
