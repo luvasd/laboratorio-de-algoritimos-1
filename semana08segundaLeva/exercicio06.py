f9ce1b2d2cd7e55a8e4b95d944cd7fced0a7cb39
luvasd <128108448+luvasd@users.noreply.github.com>
2023-05-26 17:02:31 -0300

    Update exercicio06.py

--- semana08segundaLeva/exercicio06.py
+++ semana08segundaLeva/exercicio06.py
@@ -1,11 +1,37 @@
-def somaNumNaturais(numero):
-    soma = 0
-    for i in range(1, numero + 1):
-        soma += i
-    return soma
+def obter_resposta():
+    resposta = input("Digite a resposta (sim ou não): ")
+    while resposta.lower() != "sim" and resposta.lower() != "não":
+        resposta = input("Resposta inválida. Digite novamente (sim ou não): ")
+    return resposta.lower()
 
-def main():
-    resultado = somaNumNaturais(5)
-    print(resultado)  
+def calcular_resultados():
+    total_sim = 0
+    total_nao = 0
 
-main()
+    for i in range(20):
+        resposta = obter_resposta()
+        
+        if resposta == "sim":
+            total_sim += 1
+        else:
+            total_nao += 1
+
+    return total_sim, total_nao
+
+def exibir_resultados(total_sim, total_nao):
+    print("Total de pessoas que responderam sim:", total_sim)
+    print("Total de pessoas que responderam não:", total_nao)
+
+# Função principal para executar a pesquisa
+def realizar_pesquisa():
+    print("Pesquisa de aceitação do produto")
+    print("Responda sim ou não para cada pessoa.")
+    print("----------------------------------")
+    
+    total_sim, total_nao = calcular_resultados()
+    
+    print("\n--- Resultados da Pesquisa ---")
+    exibir_resultados(total_sim, total_nao)
+
+# Chamada da função principal para iniciar a pesquisa
+realizar_pesquisa()
