ef011aef421fbfc49346009593408bcca00c3be0
luvasd <128108448+luvasd@users.noreply.github.com>
2023-06-09 17:26:34 -0300

    Create exercicio05.py

---
+++ semana10/exercicio05.py
@@ -0,0 +1,16 @@
+def criarLista():
+    lista = []
+    for i in range(10):
+        lista.append(random.randint(1, 100))
+    return lista
+
+def printarPares(lista):
+    for i in lista:
+        if i % 2 == 0:
+            print(f"- {i}")
+
+def main():
+    lista = criarLista()
+    printarPares(lista)
+
+main()
