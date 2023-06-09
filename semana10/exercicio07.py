60b4a9f49280aa903bf62ad8fa1ac293857b0495
luvasd <128108448+luvasd@users.noreply.github.com>
2023-06-09 17:27:38 -0300

    Create exercicio07.py

---
+++ semana10/exercicio07.py
@@ -0,0 +1,24 @@
+def criarLista():
+    lista = []
+    for i in range(10):
+        lista.append(random.randint(-100, 100))
+    return lista
+
+def calcQuantia(lista):
+    qtdNegativos = 0
+    somaPositivos = 0
+    for i in lista:
+        if i >= 0:
+            somaPositivos += i
+        else:
+            qtdNegativos += 1
+    return qtdNegativos, somaPositivos
+
+def main():
+    lista = criarLista()
+    print(lista)
+    qtdNegativos, somaPositivos = calcQuantia(lista)
+    print(f"A quantia de números negativos é de: {qtdNegativos} números.")
+    print(f"A soma dos números positivos desse vetor é de: {somaPositivos}.")
+
+main()
